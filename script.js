b874ae9716ac964de11439963b58a23952ef20a3
domminiq <137151864+domminiq0@users.noreply.github.com>
2023-06-22 14:48:58 +0200

    adding shape prediction and look change
    adding a field next to the board for data such as a score or next shape.
    matrices with shapes have been unified in terms of dimensions.
    the drop speed of the shapes is set using the speed field in the constructor.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
+document.querySelector('#start').addEventListener('click', () => {
+
   //A class representing the Tetris game
   class Tetris {
     constructor() {
       this.board = document.querySelector('.board');
+      this.nextShapes = document.querySelector('#nextshape');
+      this.board.innerHTML = "";
       this.boardCells = [];
       this.shapes = [];
+      this.nextShapeCells = [];
       this.currentShape = null;
       this.shapeInterval = null;
+      this.randomShape = null;
+      this.nextShape = null;
+      this.speed = 400;
     }
 
     // Draws a Tetris board
@@ -22,23 +28,68 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         this.boardCells.push(newRow);
       }
+
+      for (let row = 0; row < 4; row++) {
+        const newRow = [];
+        for (let col = 0; col < 4; col++) {
+          const cell = document.createElement('div');
+          cell.className = 'cell';
+          newRow.push(cell);
+          this.nextShapes.appendChild(cell);
+        }
+        this.nextShapeCells.push(newRow);
+      }
     }
 
     // Create available Tetris shapes
     createShapes() {
-        const shapes = [
-            [[1, 1], [1, 1]],           // Square
-            [[1, 1, 1, 1]],             // Horizontal line
-            [[1], [1], [1], [1]],       // Vertical line
-            [[1, 1, 0], [0, 1, 1]],     // Z
-            [[0, 1, 1], [1, 1, 0]],     // Reverse Z
-            [[0, 1, 0], [1, 1, 1]],     // L
-            [[1, 0, 0], [1, 1, 1]],     // Reverse L
-            [[1, 1, 1], [0, 1, 0]],     // T
-            [[0, 1, 0], [1, 1, 1]],     // Reverse T
-            [[1,0,0],[1,1,0],[1,0,0]],  // Reverse T
-            [[0,0,1],[0,1,1],[0,0,1]],  // Reverse T
+      const shapes = [
+        [[1, 1, 0, 0],        // Square
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[1, 1, 1, 1],        // Horizontal line
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[1, 0, 0, 0],        // Vertical line
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0]],
+        [[1, 1, 0, 0],       // Z
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[0, 1, 1, 0],      // Reverse Z
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[0, 1, 0, 0],      // L
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]],
+        [[0, 0, 0, 0],     // Reverse L
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0]],
+        [[1, 1, 1, 0],     // T
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[0, 1, 0, 0],    // Reverse T
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[1, 0, 0, 0],    // Reverse T
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0]],
+        [[0, 0, 1, 0],   // Reverse T
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0]],
       ];
+
 
       shapes.forEach(shapeData => {
         const shape = new Shape(shapeData);
@@ -55,6 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         }
       }
+
     }
 
     // Places the currently displayed piece on the board
@@ -66,61 +118,73 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         }
       }
+
+      for (let row = 0; row < this.nextShape.cells.length; row++) {
+        for (let col = 0; col < this.nextShape.cells[row].length; col++) {
+          if (this.nextShape.cells[row][col] === 1) {
+            const cell = this.nextShapeCells[row][col];
+            cell.classList.add('filled');
+          } else if (this.nextShape.cells[row][col] === 0) {
+            const cell = this.nextShapeCells[row][col];
+            cell.classList.remove('filled');
+          }
+        }
+      }
     }
 
     // Remove full lines from the Tetris board
     clearFilledLines() {
-        const filledRows = [];
-        for (let row = 0; row < this.boardCells.length; row++) {
-          let isFilled = true;
-          for (let col = 0; col < this.boardCells[row].length; col++) {
-            if (!this.boardCells[row][col].classList.contains('filled')) {
-              isFilled = false;
-              break;
-            }
-          }
-          if (isFilled) {
-            filledRows.push(row);
-            for (let col = 0; col < this.boardCells[row].length; col++) {
-              this.boardCells[row][col].classList.remove('filled');
-            }
+      const filledRows = [];
+      for (let row = 0; row < this.boardCells.length; row++) {
+        let isFilled = true;
+        for (let col = 0; col < this.boardCells[row].length; col++) {
+          if (!this.boardCells[row][col].classList.contains('filled')) {
+            isFilled = false;
+            break;
           }
         }
-      
-        // Responsible for moving the top cells on the Tetris board downwards after removing full lines.
-        for (const rowIndex of filledRows) {
-          for (let row = rowIndex - 1; row >= 0; row--) {
-            for (let col = 0; col < this.boardCells[row].length; col++) {
-              if (this.boardCells[row][col].classList.contains('filled')) {
-                this.boardCells[row][col].classList.remove('filled');
-                this.boardCells[row + 1][col].classList.add('filled');
-              }
+        if (isFilled) {
+          filledRows.push(row);
+          for (let col = 0; col < this.boardCells[row].length; col++) {
+            this.boardCells[row][col].classList.remove('filled');
+          }
+        }
+      }
+
+      // Responsible for moving the top cells on the Tetris board downwards after removing full lines.
+      for (const rowIndex of filledRows) {
+        for (let row = rowIndex - 1; row >= 0; row--) {
+          for (let col = 0; col < this.boardCells[row].length; col++) {
+            if (this.boardCells[row][col].classList.contains('filled')) {
+              this.boardCells[row][col].classList.remove('filled');
+              this.boardCells[row + 1][col].classList.add('filled');
             }
           }
         }
       }
-      
-      // Moves the figure down
-      moveDown() {
-        this.removeShape();
-        this.currentShape.position.y++;
-      
+    }
+
+    // Moves the figure down
+    moveDown() {
+      this.removeShape();
+      this.currentShape.position.y++;
+
+      if (this.checkCollision()) {
+        this.currentShape.position.y--;
+        this.placeShape();
+        this.currentShape = null;
+        this.clearFilledLines();
+        this.createShape();
         if (this.checkCollision()) {
-          this.currentShape.position.y--;
-          this.placeShape();
-          this.currentShape = null;
-          this.clearFilledLines(); 
-          this.createShape();
-          if (this.checkCollision()) {
-            clearInterval(this.shapeInterval);
-            alert("Game Over!");
-          } else {
-            this.placeShape();
-          }
+          clearInterval(this.shapeInterval);
+          alert("Game Over!");
         } else {
           this.placeShape();
         }
+      } else {
+        this.placeShape();
       }
+    }
 
     // Moves the figure to the right
     moveRight() {
@@ -175,9 +239,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Creates a new random piece to display on the board
     createShape() {
-      const randomShape = this.shapes[Math.floor(Math.random() * this.shapes.length)];
-      this.currentShape = Object.assign({}, randomShape);
+      if (this.randomShape == null) {
+        this.randomShape = [this.shapes[Math.floor(Math.random() * this.shapes.length)], this.shapes[Math.floor(Math.random() * this.shapes.length)]];
+      }
+      this.currentShape = Object.assign({}, this.randomShape[0]);
       this.currentShape.position = { x: 4, y: 0 };
+      this.randomShape[0] = this.randomShape[1];
+      this.randomShape[1] = this.shapes[Math.floor(Math.random() * this.shapes.length)];
+      this.nextShape = Object.assign({}, this.randomShape[0]);
+      this.nextShape.position = { x: 3, y: 0 };
     }
 
     // Starts a Tetris game
@@ -189,14 +259,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
       this.shapeInterval = setInterval(() => {
         this.moveDown();
-      }, 100);
+      }, this.speed);
 
       document.addEventListener('keydown', event => {
         if (event.key === 'ArrowRight') {
           this.moveRight();
         } else if (event.key === 'ArrowLeft') {
           this.moveLeft();
-        } 
+        }
       });
     }
   }
